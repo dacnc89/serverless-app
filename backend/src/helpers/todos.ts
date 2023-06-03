01dd0afd3595270f887dfacb855b03503b00646d
dacnc <congdacit@gmail.com>
2023-06-04 02:00:09 +0700

    add get todos function

--- backend/src/helpers/todos.ts
+++ backend/src/helpers/todos.ts
@@ -1,10 +1,18 @@
-// import { TodosAccess } from './todosAcess'
+import { TodosAcess } from './todosAcess'
 // import { AttachmentUtils } from './attachmentUtils';
-// import { TodoItem } from '../models/TodoItem'
+import { TodoItem } from '../models/TodoItem'
 // import { CreateTodoRequest } from '../requests/CreateTodoRequest'
 // import { UpdateTodoRequest } from '../requests/UpdateTodoRequest'
-// import { createLogger } from '../utils/logger'
+import { createLogger } from '../utils/logger'
 // import * as uuid from 'uuid'
 // import * as createError from 'http-errors'
 
 // TODO: Implement businessLogic
+
+const logger = createLogger('Todos')
+export async function getTodosForUser(user: string): Promise<TodoItem[]> {
+  logger.info('In function: getTodosForUser()')
+
+  return await TodosAcess.getTodosForUser(user)
+}
+
--- backend/src/helpers/todosAcess.ts
+++ backend/src/helpers/todosAcess.ts
@@ -1,12 +1,40 @@
-// import * as AWS from 'aws-sdk'
+import * as AWS from 'aws-sdk'
 // import * as AWSXRay from 'aws-xray-sdk'
-// import { DocumentClient } from 'aws-sdk/clients/dynamodb'
-// import { createLogger } from '../utils/logger'
-// import { TodoItem } from '../models/TodoItem'
+import { DocumentClient } from 'aws-sdk/clients/dynamodb'
+import { createLogger } from '../utils/logger'
+import { TodoItem } from '../models/TodoItem'
 // import { TodoUpdate } from '../models/TodoUpdate';
 
 // const XAWS = AWSXRay.captureAWS(AWS)
 
-// const logger = createLogger('TodosAccess')
+const logger = createLogger('TodosAccess')
 
-// // TODO: Implement the dataLayer logic
+// TODO: Implement the dataLayer logic
+export class TodosAcess {
+  constructor(
+    private readonly docClient: DocumentClient = createDynamoDBClient(),
+    private readonly TodosTable = process.env.TODOS_TABLE
+  ) {}
+
+  async getTodosForUser(userId: string): Promise<TodoItem[]> {
+    logger.info('In function: getTodosForUser()')
+    const result = await this.docClient
+      .query({
+        TableName: this.TodosTable,
+        KeyConditionExpression: '#userId = :i',
+        ExpressionAttributeNames: {
+          '#userId': 'userId'
+        },
+        ExpressionAttributeValues: {
+          ':i': userId
+        }
+      })
+      .promise()
+    return result.Items as TodoItem[]
+  }
+}
+
+/** Create Dynamo Db */
+function createDynamoDBClient() {
+  return new AWS.DynamoDB.DocumentClient()
+}
